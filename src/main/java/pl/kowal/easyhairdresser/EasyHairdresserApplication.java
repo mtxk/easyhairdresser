6f5c4be536adf42c09c7056a7e620068d2b20615
Mateusz Kowal <mateuszxkowal@gmail.com>
2019-10-23 23:27:17 +0200

    chore(style): proper names

--- src/main/java/pl/kowal/easyhairdresser/EasyHairdresserApplication.java
+++ src/main/java/pl/kowal/easyhairdresser/EasyHairdresserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasyhairdresserApplication {
+public class EasyHairdresserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EasyhairdresserApplication.class, args);
+		SpringApplication.run(EasyHairdresserApplication.class, args);
 	}
 
 }
