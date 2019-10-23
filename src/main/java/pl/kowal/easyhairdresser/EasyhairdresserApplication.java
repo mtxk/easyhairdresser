62de6e60ff0c76add351b99377ec0d7bbc033afe
Mateusz Kowal <mateuszxkowal@gmail.com>
2019-10-23 23:19:55 +0200

    init

---
+++ src/main/java/pl/kowal/easyhairdresser/EasyhairdresserApplication.java
@@ -0,0 +1,13 @@
+package pl.kowal.easyhairdresser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyhairdresserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyhairdresserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kowal/easyhairdresser/EasyhairdresserApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kowal.easyhairdresser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyhairdresserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
